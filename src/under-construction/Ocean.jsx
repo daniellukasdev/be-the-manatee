dabf8e58f575dca34545d47f7fdffe0b05953682
Daniel Lukas <dtlukasart@gmail.com>
2023-03-12 11:46:54 -0400

    converted JS to JSX extension and some css

---
+++ src/under-construction/Ocean.jsx
@@ -0,0 +1,11 @@
+import "./Ocean.css";
+import Wave from "./Wave";
+
+export default function Ocean() {
+    return (
+    <div className="ocean">
+        <Wave />
+        <Wave />
+    </div>
+    );
+}
---
+++ src/under-construction/Wave.jsx
@@ -0,0 +1,5 @@
+import "./Wave.css";
+
+export default function Wave() {
+    return <div className="wave"></div>;
+}
