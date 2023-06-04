a6299f3337c63c1accb052b35a3381dc7089bdda
Daniel Lukas <dtlukasart@gmail.com>
2023-06-04 18:29:01 -0400

    logo title and some css tweaks

--- src/under-construction/UnderConstruction.jsx
+++ src/under-construction/UnderConstruction.jsx
@@ -1,9 +1,11 @@
 import BeTheManateeLogo from "../BeTheManateeLogo/BeTheManateeLogo";
+import Title from "../BeTheManateeLogo/Title";
 import "./UnderConstruction.css";
 
 export default function UnderConstruction() {
   return (
     <div className="under-construction">
+      <Title titleClass="logo-title" />
       <div className="under-construction-header">
       </div>
       <div className="under-construction-body">
